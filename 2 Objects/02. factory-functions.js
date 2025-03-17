d0890d0437cda1df9075817ed97914b5f4b172c3
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2025-03-17 19:25:35 +0530

    Factory Function

--- 2 Objects/02. factory-functions.js
+++ 2 Objects/02. factory-functions.js
@@ -1,1 +1,25 @@
-// =================================================================================================
+// =====================================================================================================================
+// Factory Function is a function that returns a new object. 
+// It is used to create multiple similar objects without using classes or the new keyword.
+// =====================================================================================================================
+
+function createPerson(name, school) {
+    return{
+        name,
+        school,
+        goingToSchool() {
+            console.log(`${this.name} is going to ${this.school}`);
+        },
+        doingHomeWork() {
+            console.log(`${this.name} is doing homework`);
+        }
+    }
+}
+
+student01 = createPerson("Hasindu", "Nalanda College"); // Create a new object using the factory function
+student02 = createPerson("Lakshan", "Dharmaraja College"); // Create a new object using the factory function
+
+console.log( student01.name)
+student01.goingToSchool();
+
+
